2cd9a29e6a79805077013e72bad8ce053464729e
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2023-02-10 10:18:45 -0500

    push

--- HotelManagement/src/controller/MainMenuController.java
+++ HotelManagement/src/controller/MainMenuController.java
@@ -1,6 +1,7 @@
 package controller;
 import view.MainMenu;
 import view.customer.CustomerScreen;
+import view.room.RoomCard;
 import view.DefaultScreen;
 
 import java.awt.event.ActionEvent;
@@ -30,6 +31,16 @@ public class MainMenuController implements ActionListener {
 
 		if(e.getSource() == main.getRoomButton()){
 			ApplicationController.screenSwitching(ApplicationController.screens[TypeOfScreen.ROOM_SCREEN.getNumber()]);
+			//Example on how to create a room card. This should be moved to a controller class and this comment removed.
+        /*RoomCard roomCard1 = new RoomCard();
+        RoomCard roomCard2 = new RoomCard();
+        RoomCard roomCard3 = new RoomCard();
+
+		container.add(roomCard1);
+        container.add(roomCard2);
+        container.add(roomCard3);
+*/
+         
 		}else if(e.getSource() == main.getRequestButton()){
 			ApplicationController.screenSwitching(ApplicationController.screens[TypeOfScreen.REGISTER_SCREEN.getNumber()]);
 		}
@@ -37,3 +48,4 @@ public class MainMenuController implements ActionListener {
 	}
 
 }
+////push
